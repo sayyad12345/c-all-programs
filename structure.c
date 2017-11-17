081d034d9b7d3790983df0b7c69e46c773e2d7e1
sayyad12345 <33539134+sayyad12345@users.noreply.github.com>
2017-11-17 06:51:38 +0000

    Add files via upload

---
+++ structure.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+ 
+struct student 
+{
+           int id;
+           char name[20];
+           float percentage;
+};
+ 
+int main() 
+{
+           struct student record = {0}; //Initializing to null
+ 
+           record.id=1;
+           strcpy(record.name, "salim");
+           record.percentage = 86.5;
+ 
+           printf(" Id is: %d \n", record.id);
+           printf(" Name is: %s \n", record.name);
+           printf(" Percentage is: %f \n", record.percentage);
+           return 0;
+}
